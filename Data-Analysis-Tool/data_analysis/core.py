47b6bdcccfc6ea2329b963b875c830c7f966240f
E23026 <e23026@eng.pdn.ac.lk>
2026-05-30 18:03:55 +0530

    Update core.py

--- Data-Analysis-Tool/data_analysis/core.py
+++ Data-Analysis-Tool/data_analysis/core.py
@@ -1,1 +1,19 @@
-# Core classes will go here
+import pandas as pd
+import numpy as np
+
+from google.colab import files
+import io
+
+import plotly.express as px
+import plotly.graph_objects as go
+
+from plotly.subplots import make_subplots
+
+from sklearn.preprocessing import (
+    MinMaxScaler,
+    StandardScaler,
+    RobustScaler,
+    OrdinalEncoder
+)
+
+from scipy.stats import chi2_contingency
